dad70d79fd1ea57272da07604751e9b7fc28459d
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2022-01-31 14:21:56 +0200

    Initial commit

---
+++ GraphAlgorithms/GraphsOperations.cs
@@ -0,0 +1,26 @@
+﻿namespace GraphAlgorithms;
+
+public class GraphsOperations<T>
+{
+    #region Get Depth First Values
+
+    public static List<T> GetDepthFirstValues(Dictionary<T, List<T>> graph, T entry)
+    {
+        var resultList = new List<T>();
+        var stack = new Stack<T>();
+        stack.Push(entry);
+
+        while (stack.Count > 0)
+        {
+            var currentEntry = stack.Pop();
+            resultList.Add(currentEntry);
+
+            var currentList = graph[currentEntry];
+            currentList.ForEach(entry => stack.Push(entry));
+        }
+
+        return resultList;
+    }
+
+    #endregion
+}
---
+++ GraphAlgorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿#region Preparing Testing Graph
+
+using GraphAlgorithms;
+
+var graph = new Dictionary<string, List<string>>
+{
+    {"a", new List<string> {"b", "c"}},
+    {"b", new List<string> {"d"}},
+    {"c", new List<string> {"e"}},
+    {"d", new List<string> {"f"}},
+    {"e", new List<string>()},
+    {"f", new List<string>()}
+};
+
+#endregion
+
+#region Testing
+
+PrintCollection(GraphsOperations<string>.GetDepthFirstValues(graph, "a"));
+
+#endregion
+
+#region helpers
+
+static void PrintCollection<T>(ICollection<T> collection, string name = "")
+{
+    Console.WriteLine($"------------Start {name}------------");
+    foreach (var item in collection)
+    {
+        Console.WriteLine(item);
+    }
+
+    Console.WriteLine($"------------End {name}------------");
+}
+
+#endregion
